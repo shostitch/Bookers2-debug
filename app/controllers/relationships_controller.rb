df172e1b1a74e987985deb537d7d6dc365c052e3
EC2 Default User <ec2-user@ip-172-31-95-126.ec2.internal>
2022-05-26 04:29:22 +0000

    完成

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,14 +1,12 @@
 class RelationshipsController < ApplicationController
 
   def create
-    user = User.find(params[:user_id])
-    current_user.follow(user)
+    current_user.follow(params[:user_id])
     redirect_to request.referer
   end
 
   def destroy
-    user = User.find(params[:user_id])
-    current_user.unfollow(user)
+    current_user.unfollow(params[:user_id])
     redirect_to request.referer
   end
 
